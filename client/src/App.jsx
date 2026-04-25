df2f9128508e0763ad95d05c4bdaea3fa1bc81e2
ismaelaboud <smail.abud@gmail.com>
2026-04-25 15:57:28 +0300

    add web analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Leaderboard from './pages/Leaderboard'
 import History from './pages/History'
 import Admin from './pages/Admin'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/history" element={<History />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
+      <Analytics />
     </div>
   )
 }
